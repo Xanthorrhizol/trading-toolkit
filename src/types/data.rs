6737ae9421b83823d1f4b0f4d6921a81e3d3f558
Xanthorrhizol <xanthorrhizol@proton.me>
2023-05-14 11:57:35 +0900

    F: BaseData parsing for Stock

--- src/types/data.rs
+++ src/types/data.rs
@@ -13,3 +13,16 @@ pub trait Stock {
     fn tot_exec_volume(&self) -> u64;
     fn epoch_time(&self) -> u128;
 }
+
+impl BaseData for dyn Stock {
+    fn value(&self) -> f64 {
+        self.close_price()
+    }
+
+    fn weight(&self) -> u64 {
+        self.tot_exec_volume()
+    }
+    fn epoch_time(&self) -> u128 {
+        self.epoch_time()
+    }
+}
